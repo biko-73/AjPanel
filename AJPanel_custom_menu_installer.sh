c8cc0eacd2f1ac125c32d5a12fd59b82dfde8add
biko-73 <57875905+biko-73@users.noreply.github.com>
2023-12-28 05:15:26 +0200

    Add files via upload

---
+++ AJPanel_custom_menu_installer.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# ============================================================================================================
+# SCRIPT : Download/Install AJPanel Custom-Menu XML Files
+# ============================================================================================================
+
+BACKUP_DIR=$(cat /etc/enigma2/settings | grep config.plugins.AJPanel.backupPath | cut -d '=' -f 2)
+
+for FNAME in "ajpanel_menu_biko_73.xml"  "ajpanel_menu_Emil.xml"  "ajpanel_menu_Haitham.xml"  "ajpanel_menu.xml"
+do
+	echo "--------------------------------------------------"
+	echo ">>>>>  Downloading $FNAME  <<<<<"
+	wget --no-check-certificate -O $BACKUP_DIR$FNAME https://raw.githubusercontent.com/biko-73/AjPanel/main/$FNAME
+	echo ''
+done
